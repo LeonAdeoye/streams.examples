def1677eb3c679eacdfb1c079e4c5f7f19272d52
LeonAdeoye <leon.adeoye@gmail.com>
2021-05-08 23:04:27 +0800

    Pipe setup

--- src/main/java/com/leon/ks/Pipe.java
+++ src/main/java/com/leon/ks/Pipe.java
@@ -1,12 +1,14 @@
 package com.leon.ks;
 
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.KStream;
 
 import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
 
 public class Pipe
 {
@@ -37,5 +39,30 @@ public class Pipe
         final Topology topology = builder.build();
         // And print its description to standard output as:
         System.out.println(topology.describe());
+
+        final KafkaStreams streams = new KafkaStreams(topology, props);
+        final CountDownLatch latch = new CountDownLatch(1);
+
+        // attach shutdown handler to catch control-c
+        Runtime.getRuntime().addShutdownHook(new Thread("streams-shutdown-hook")
+        {
+            @Override
+            public void run()
+            {
+                streams.close();
+                latch.countDown();
+            }
+        });
+
+        try
+        {
+            streams.start();
+            latch.await();
+        }
+        catch (Throwable e)
+        {
+            System.exit(1);
+        }
+        System.exit(0);
     }
 }
